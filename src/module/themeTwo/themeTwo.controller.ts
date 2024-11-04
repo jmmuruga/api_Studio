f975947050a7052c3c4a603e40ecb1b29873b2f8
arunprasanthm7 <arunprasanthm7@gmail.com>
2024-11-04 18:06:21 +0530

    dynamic path is implemented in photography page and api work in gallery page

--- src/module/themeTwo/themeTwo.controller.ts
+++ src/module/themeTwo/themeTwo.controller.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAlbumImages, getAlbumName, getPhotographyTypeServices } from "./themeTwo.service";
+import { getAlbumImages, getAlbumName, getAllImages, getPhotographyTypeServices } from "./themeTwo.service";
 
 const themeTwoRouter = Router();
 themeTwoRouter.get('/getPhotographyTypeServices',(req,res)=>{getPhotographyTypeServices(req,res)});
 themeTwoRouter.get('/getAlbumName/:album_name',(req,res)=>{getAlbumName(req,res)});
 themeTwoRouter.get('/getAlbumImages/:albumid',(req,res)=>{getAlbumImages(req,res)});
+themeTwoRouter.get('/getAllImages',(req,res)=>{getAllImages(req,res)});
+
 
 export default themeTwoRouter;
--- src/module/themeTwo/themeTwo.service.ts
+++ src/module/themeTwo/themeTwo.service.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { galleryMaster } from "../gallery/gallery.model";
+import { galleryMaster, galleryMasterNested } from "../gallery/gallery.model";
 import { appSource } from "../../core/db";
 import { galleryDetailsDto } from "../gallery/gallery.dto";
 import { ValidationException } from "../../core/exception";
@@ -8,11 +8,23 @@ export const getPhotographyTypeServices = async (
   req: Request,
   res: Response
 ) => {
-  console.log("called");
+  // console.log("called");
   try {
     const galleryMasterRepository = appSource.getRepository(galleryMaster);
     const details: galleryDetailsDto[] = await galleryMasterRepository.query(`
-        select album_name from [${process.env.DB_NAME}].[dbo].[gallery_master] where isactive=1 group by album_name
+        WITH getFirstImg AS (
+    SELECT CAST(baseimg AS VARCHAR(MAX)) AS baseimg,albumid,
+        ROW_NUMBER() OVER (PARTITION BY albumid ORDER BY photoid) AS row_num
+    FROM [${process.env.DB_NAME}].[dbo].[gallery_master_nested]
+    WHERE isactive = 1
+)
+
+SELECT gm.album_name, ISNULL(gmn.baseimg, '') AS baseimg
+FROM  [${process.env.DB_NAME}].[dbo].[gallery_master] gm
+LEFT JOIN getFirstImg gmn ON gmn.albumid = gm.albumid AND gmn.row_num = 1
+WHERE gm.isactive = 1
+GROUP BY gm.album_name,gmn.baseimg;
+
         `);
 
     res.status(200).send({
@@ -31,7 +43,7 @@ export const getPhotographyTypeServices = async (
 
 export const getAlbumName = async (req: Request, res: Response) => {
   const albumName = req.params.album_name;
-  console.log("calledServices", albumName);
+  // console.log("calledServices", albumName);
   try {
     const galleryMasterRepository = appSource.getRepository(galleryMaster);
     const details = await galleryMasterRepository
@@ -41,7 +53,6 @@ export const getAlbumName = async (req: Request, res: Response) => {
       })
       .andWhere("galleryMaster.isactive = :isactive", { isactive: true })
       .getMany();
-
     res.status(200).send({
       Result: details,
     });
@@ -58,7 +69,7 @@ export const getAlbumName = async (req: Request, res: Response) => {
 
 export const getAlbumImages = async (req: Request, res: Response) => {
   const albumId = req.params.albumid;
-  console.log("calledAlbumId", albumId);
+  // console.log("calledAlbumId", albumId);
   try {
     const galleryMasterRepository = appSource.getRepository(galleryMaster);
     const details: galleryDetailsDto[] = await galleryMasterRepository.query(`
@@ -67,6 +78,29 @@ inner join [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn on gm.albu
 where gm.isactive = 1 and gm.albumid = ${albumId}
         `);
 
+    res.status(200).send({
+      Result: details,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+export const getAllImages = async (req: Request, res: Response) => {
+  try {
+    const repo = appSource.getRepository(galleryMasterNested);
+    const details = await repo 
+      .createQueryBuilder("galleryMasterNested")
+      .where("galleryMasterNested.isactive = :isactive", {
+        isactive: true,
+      })
+      .getMany();
     res.status(200).send({
       Result: details,
     });
