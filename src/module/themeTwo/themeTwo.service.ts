88b357c78c2df59f32297b0017cb6e1ef4982e44
arunprasanthm7 <arunprasanthm7@gmail.com>
2024-11-16 17:17:44 +0530

    changes

--- src/module/themeTwo/themeTwo.service.ts
+++ src/module/themeTwo/themeTwo.service.ts
@@ -15,7 +15,7 @@ export const getPhotographyTypeServices = async (
     const details = await galleryMasterRepository.query(`
       SELECT gm.album_name
       FROM [${process.env.DB_NAME}].[dbo].[gallery_master] gm
-      WHERE gm.isactive = 1
+      WHERE gm.isdelete = 1
       GROUP BY gm.album_name`);
 
     for (const album of details) {
@@ -24,8 +24,8 @@ export const getPhotographyTypeServices = async (
           SELECT TOP 1 gm.album_name, CAST(gmn.baseimg AS VARCHAR(MAX)) AS baseimg
           FROM [${process.env.DB_NAME}].[dbo].[gallery_master] gm
           INNER JOIN [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn 
-            ON gmn.albumid = gm.albumid AND gmn.isactive = 1
-          WHERE gm.isactive = 1 AND gm.album_name = '${album.album_name}'
+            ON gmn.albumid = gm.albumid AND gmn.isdelete = 1
+          WHERE gm.isdelete = 1 AND gm.album_name = '${album.album_name}'
           ORDER BY gmn.photoid
         `
       );
@@ -55,14 +55,14 @@ export const getAlbumName = async (req: Request, res: Response) => {
     const details = await galleryMasterRepository.query(`
      SELECT *
 FROM  [${process.env.DB_NAME}].[dbo].[gallery_master] gm
-WHERE gm.isactive = 1 and gm.album_name = '${albumName}'`);
+WHERE gm.isdelete = 1 and gm.album_name = '${albumName}'`);
 
     for (const album of details) {
       const nestedData = await galleryMasterRepository.query(
         `SELECT top 1 gm.album_name, CAST(gmn.baseimg AS VARCHAR(MAX)) AS baseimg
 FROM [${process.env.DB_NAME}].[dbo].[gallery_master] gm
-inner JOIN [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn ON gmn.albumid = gm.albumid and gmn.isactive=1
-WHERE gm.isactive = 1 and gm.title='${album.title}'
+inner JOIN [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn ON gmn.albumid = gm.albumid and gmn.isdelete=1
+WHERE gm.isdelete = 1 and gm.title='${album.title}'
  ORDER BY gmn.photoid`
       );
 
@@ -90,15 +90,15 @@ export const getAlbumImages = async (req: Request, res: Response) => {
     const galleryMasterRepository = appSource.getRepository(galleryMaster);
     const details: galleryDetailsDto[] = await galleryMasterRepository.query(`
         select gmn.baseimg,gm.title,gm.album_name from [${process.env.DB_NAME}].[dbo].[gallery_master] gm
-inner join [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn on gm.albumid = gmn.albumid and gmn.isactive = 1
-where gm.isactive = 1 and gm.albumid = ${albumId}
+inner join [${process.env.DB_NAME}].[dbo].[gallery_master_nested] gmn on gm.albumid = gmn.albumid and gmn.isdelete = 1
+where gm.isdelete = 1 and gm.albumid = ${albumId}
         `);
 
     res.status(200).send({
       Result: details,
     });
   } catch (error) {
-    console.log('geall',error);
+    console.log("geall", error);
     if (error instanceof ValidationException) {
       return res.status(400).send({
         message: error?.message,
@@ -113,8 +113,8 @@ export const getAllImages = async (req: Request, res: Response) => {
     const repo = appSource.getRepository(galleryMasterNested);
     const details = await repo
       .createQueryBuilder("galleryMasterNested")
-      .where("galleryMasterNested.isactive = :isactive", {
-        isactive: true,
+      .where("galleryMasterNested.isdelete = :isdelete", {
+        isdelete: true,
       })
       .getMany();
     res.status(200).send({
@@ -136,8 +136,8 @@ export const getAllImages = async (req: Request, res: Response) => {
 //   try {
 //     const galleryMasterRepository = appSource.getRepository(bannerMaster);
 //     const details: bannerDetailsDto[] =
-//       await galleryMasterRepository.query(`select * from  [${process.env.DB_NAME}].[dbo].[banner_master] bm 
-// inner join  [${process.env.DB_NAME}].[dbo].[banner_master_nested] bmn on bmn.bannerid = bm.bannerid 
+//       await galleryMasterRepository.query(`select * from  [${process.env.DB_NAME}].[dbo].[banner_master] bm
+// inner join  [${process.env.DB_NAME}].[dbo].[banner_master_nested] bmn on bmn.bannerid = bm.bannerid
 // where bm.menu_name = '${pageName}'
 //         `);
 
