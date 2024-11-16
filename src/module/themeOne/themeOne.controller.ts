b120ab78caee70e5acaaa6cc947a53c604061ded
Saranya6676 <saranyachandrabose27@gmail.com>
2024-11-16 17:22:10 +0530

    get a images using api  for home page

--- src/module/themeOne/themeOne.controller.ts
+++ src/module/themeOne/themeOne.controller.ts
@@ -1,7 +1,9 @@
 import {Router} from "express";
 import { getGalleryListAsMenu } from "./themeOne.service";
+import { auth } from "../../shared/helper";
 
 const themeOneRouter = Router();
-themeOneRouter.get("/getAllImages",(req,res)=>{getGalleryListAsMenu(req,res)});
+themeOneRouter.get("/getAllImages" ,(req,res)=>{getGalleryListAsMenu(req,res)});
 
 export default themeOneRouter; 
+
--- src/module/themeOne/themeOne.service.ts
+++ src/module/themeOne/themeOne.service.ts
@@ -1,33 +1,37 @@
-import { Request,Response} from "express";
-import { galleryMaster } from "../gallery/gallery.model";
+import { Request, Response } from "express";
+import { galleryMaster, galleryMasterNested } from "../gallery/gallery.model";
 import { galleryDetailsDto } from "../gallery/gallery.dto";
 import { ValidationException } from "../../core/exception";
 import { appSource } from "../../core/db";
 
-
 export const getGalleryListAsMenu = async (req: Request, res: Response) => {
-    console.log('called')
-    try {
-        const galleryMasterRepository = appSource.getRepository(galleryMaster);
-        const details: galleryDetailsDto[] = await galleryMasterRepository.query(`
-    with getCount as (
-        select count(albumid) as counts,albumid from [${process.env.DB_NAME}].[dbo].[gallery_master_nested] where isdelete=1
-        group by albumid
-        )
-        
-        select gm.*,gmncount.counts from [${process.env.DB_NAME}].[dbo].[gallery_master] gm
-        left join getCount gmncount on gmncount.albumid=gm.albumid
-        where gm.isdelete=1
-  `);
-        res.status(200).send({
-            Result: details
-        });
-    } catch (error) {
-        if (error instanceof ValidationException) {
-            return res.status(400).send({
-                message: error?.message,
-            });
-        }
-        res.status(500).send(error);
+  try {
+    // const repo = appSource.getRepository(galleryMasterNested);
+    // const galleryMasterNesteddetails = await repo
+    //   .createQueryBuilder("galleryMasterNested")
+    //   .where("galleryMasterNested.isdelete=:isdelete", { isdelete: true })
+    //   .getMany();
+
+    // const galleryR
+    // res.status(200).send({
+    //   Result: galleryMasterNesteddetails,
+    // });
+
+    const galleryMasterRepo = appSource.getRepository(galleryMaster);
+    const galleryMasterDetails = await galleryMasterRepo.query(`
+        select gmn.baseimg from [${process.env.DB_NAME}].[DBO].[gallery_master] as gm
+        inner join [${process.env.DB_NAME}].[DBO].[gallery_master_nested] as gmn on gm.albumid = gmn.albumid
+        where gm.isdelete = 1 and gm.status=1`)
+
+    res.status(200).send({
+      Result: galleryMasterDetails,
+    });
+  } catch (error) {
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
     }
+    res.status(500).send(error);
+  }
 };
