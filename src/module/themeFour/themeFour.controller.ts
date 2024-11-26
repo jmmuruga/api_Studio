47a90c1fbd40bda5d1db31bcc2c00e87406c1b8a
Akshayachuz <akshayamanju2@gmail.com>
2024-11-26 18:16:56 +0530

    company, blog, blogtype api

--- src/module/themeFour/themeFour.controller.ts
+++ src/module/themeFour/themeFour.controller.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { auth } from "../../shared/helper";
-import { getBannerImages, getCompanyDetails, getServices } from "./themeFour.service";
+import { getBannerImages, getBlogDetails, getBlogImages, getCompanyDetails, getServices } from "./themeFour.service";
 
 const themeFourRouter = Router ();
 themeFourRouter.get('/themeFourBannerImages/:page', (req, res) => (getBannerImages(req, res)) );
 themeFourRouter.get(`/themeFourHomeServices`, (req, res) => (getServices(req, res)) );
 themeFourRouter.get(`/themeFourCompanyDetails`,(req, res) => (getCompanyDetails(req, res)));
+themeFourRouter.get('/themeFourBlogDetails', (req, res) => (getBlogDetails(req, res)));
+themeFourRouter.get('/themeFourBlogImages/:albumid', (req, res) => (getBlogImages(req, res)));
 export default themeFourRouter;
--- src/module/themeFour/themeFour.service.ts
+++ src/module/themeFour/themeFour.service.ts
@@ -5,6 +5,8 @@ import { bannerDetailsDto } from "../banner/banner.dto";
 import { ValidationException } from "../../core/exception";
 import { companyDetails } from "../company/companyDetails.model";
 import { companyDetailsDto } from "../company/companyDetails.dto";
+import { galleryMaster } from "../gallery/gallery.model";
+import { galleryDetailsDto } from "../gallery/gallery.dto";
 
 export const getBannerImages = async (req: Request, res: Response) => {
   const menuName = req.params.page;
@@ -52,6 +54,52 @@ export const getCompanyDetails = async (req: Request, res: Response) => {
     const details: companyDetailsDto[] = await companyDetailsRepo.query(
       `select company_name, e_mail, mobile, whats_app, address, logo from company_details  `
     );
+    res.status(200).send({ Result: details });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+export const getBlogDetails = async (req: Request, res: Response) => {
+  try {
+    const galleryMasterRepo = appSource.getRepository(galleryMaster);
+    const details: galleryDetailsDto[] = await galleryMasterRepo.query(
+      `SELECT gm.albumid, gm.album_name, gm.title, 
+      CAST(gmn.baseimg AS VARCHAR(MAX)) AS baseimg
+FROM [${process.env.DB_name}].[dbo].[gallery_master] gm
+INNER JOIN [${process.env.DB_name}].[dbo].[gallery_master_nested] gmn ON gm.albumid = gmn.albumid
+WHERE gm.isdelete = 1 AND gmn.isdelete = 1
+GROUP BY gm.albumid, gm.album_name, gm.title, CAST(gmn.baseimg AS VARCHAR(MAX));
+ `
+    );
+    res.status(200).send({ Result: details });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+export const getBlogImages = async (req: Request, res: Response) => {
+  const albumId = req.params.albumid;
+  console.log(albumId,"albumId")
+  try {
+    const galleryMasterRepo = appSource.getRepository(galleryMaster);
+    const details: galleryDetailsDto[] =
+      await galleryMasterRepo.query(`select baseimg from [${process.env.DB_name}].[dbo].[gallery_master] gm 
+inner join [${process.env.DB_name}].[dbo].[gallery_master_nested] gmn on gm.albumid = gmn.albumid 
+where gm.isdelete = 1 and gmn.isdelete = 1 and gm.albumid = ${albumId}
+ `);
     res.status(200).send({ Result: details });
   } catch (error) {
     console.log(error);
