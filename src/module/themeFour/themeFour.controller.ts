7ddde82666e6ea059cf31317b86fcc6a9971c8c4
Akshayachuz <akshayamanju2@gmail.com>
2024-11-30 18:19:32 +0530

    changes

--- src/module/themeFour/themeFour.controller.ts
+++ src/module/themeFour/themeFour.controller.ts
@@ -1,15 +1,47 @@
 import { Router } from "express";
 import { auth } from "../../shared/helper";
-import { getAllServices, getBannerImages, getBlogDetails, getBlogImages, getCompanyDetails, getHomePageServices, getServices, getServicesType } from "./themeFour.service";
+import {
+  getAllServices,
+  getBannerImages,
+  getBlogDetails,
+  getBlogImages,
+  getCompanyDetails,
+  getGalleryImages,
+  getHomePageServices,
+  getServices,
+  getServicesType,
+  sendMail,
+} from "./themeFour.service";
 
-const themeFourRouter = Router ();
-themeFourRouter.get('/themeFourBannerImages/:page', (req, res) => (getBannerImages(req, res)) );
-themeFourRouter.get(`/themeFourHomeServices`, (req, res) => (getServices(req, res)) );
-themeFourRouter.get(`/themeFourCompanyDetails`,(req, res) => (getCompanyDetails(req, res)));
-themeFourRouter.get('/themeFourBlogDetails', (req, res) => (getBlogDetails(req, res)));
-themeFourRouter.get('/themeFourBlogImages/:albumid', (req, res) => (getBlogImages(req, res)));
-themeFourRouter.get('/themeFourServicesType' , (req, res) => (getServicesType(req, res)));
-themeFourRouter.get('/themeFourAllServices/:title/:albumid', (req, res) =>(getAllServices(req, res)));
-themeFourRouter.get('/getHomePageServices', (req, res) => (getHomePageServices(req, res)));
-
+const themeFourRouter = Router();
+themeFourRouter.get("/themeFourBannerImages/:page", (req, res) =>
+  getBannerImages(req, res)
+);
+themeFourRouter.get(`/themeFourHomeServices`, (req, res) =>
+  getServices(req, res)
+);
+themeFourRouter.get(`/themeFourCompanyDetails`, (req, res) =>
+  getCompanyDetails(req, res)
+);
+themeFourRouter.get("/themeFourBlogDetails", (req, res) =>
+  getBlogDetails(req, res)
+);
+themeFourRouter.get("/themeFourBlogImages/:albumid", (req, res) =>
+  getBlogImages(req, res)
+);
+themeFourRouter.get("/themeFourServicesType", (req, res) =>
+  getServicesType(req, res)
+);
+themeFourRouter.get("/themeFourAllServices/:title/:albumid", (req, res) =>
+  getAllServices(req, res)
+);
+themeFourRouter.get("/themeFourgetHomePageServices", (req, res) =>
+  getHomePageServices(req, res)
+);
+themeFourRouter.get("/themeFourgetGalleryImages", (req, res) =>
+  getGalleryImages(req, res)
+);
+themeFourRouter.post("/sendMail", (req, res) => {
+  sendMail(req, res);
+});
 export default themeFourRouter;
--- src/module/themeFour/themeFour.service.ts
+++ src/module/themeFour/themeFour.service.ts
@@ -7,6 +7,8 @@ import { companyDetails } from "../company/companyDetails.model";
 import { companyDetailsDto } from "../company/companyDetails.dto";
 import { galleryMaster } from "../gallery/gallery.model";
 import { galleryDetailsDto } from "../gallery/gallery.dto";
+import nodemailer from "nodemailer";
+// import { formDetails } from "../formDetails/formDetails.model";
 
 export const getBannerImages = async (req: Request, res: Response) => {
   const menuName = req.params.page;
@@ -69,12 +71,15 @@ export const getBlogDetails = async (req: Request, res: Response) => {
   try {
     const galleryMasterRepo = appSource.getRepository(galleryMaster);
     const details: galleryDetailsDto[] = await galleryMasterRepo.query(
-      `SELECT gm.albumid, gm.album_name, gm.title, 
-       MIN(CAST(gmn.baseimg AS VARCHAR(MAX))) AS baseimg
-FROM [${process.env.DB_name}].[dbo].[gallery_master] gm
-INNER JOIN [${process.env.DB_name}].[dbo]. [gallery_master_nested] gmn ON gm.albumid = gmn.albumid
-WHERE gm.isdelete = 1 AND gmn.isdelete = 1 AND gm.album_name = 'blog'
-GROUP BY gm.albumid, gm.album_name, gm.title;
+      `SELECT bm.bannerid, 
+       bmn.title, 
+       CAST(bmn.baseimg AS VARCHAR(MAX)) AS baseimg, 
+	   CAST( bmn.description AS VARCHAR(MAX)) AS description, 
+	   bm.menu_name 
+FROM banner_master bm
+INNER JOIN banner_master_nested bmn ON bm.bannerid = bmn.bannerid
+where bm.menu_name = 'blog'
+GROUP BY bm.bannerid, bmn.title,  CAST(bmn.baseimg AS VARCHAR(MAX)),  CAST( bmn.description AS VARCHAR(MAX)) , bm.menu_name;
  `
     );
     res.status(200).send({ Result: details });
@@ -117,7 +122,6 @@ export const getServicesType = async (req: Request, res: Response) => {
       await galleryMasterRepo.query(`select gm.albumid, gm.title, max(cast(gmn.baseimg as varchar(max))) as baseimg from [${process.env.DB_name}].[dbo].[gallery_master] gm 
 inner join [${process.env.DB_name}].[dbo].[gallery_master_nested] gmn on gm.albumid = gmn.albumid
 where gm.isdelete = 1 and gmn.isdelete = 1
-and (gm.album_name = 'wedding' OR gm.album_name = 'outdoor' OR gm.album_name = 'commercial')
 group by  gm.albumid, gm.title; 
  `);
     res.status(200).send({ Result: details });
@@ -163,8 +167,9 @@ group by album_name;
       `);
 
     for (const album of albumList) {
+      console.log(album.album_name , 'name')
       album.photos =
-        await galleryMasterRepo.query(`SELECT gm.albumid, gm.album_name,
+        await galleryMasterRepo.query(`SELECT top 3 gm.albumid, gm.album_name,
        gm.title, 
        MAX(CAST(gmn.baseimg AS VARCHAR(MAX))) AS baseimg, 
        gm.description
@@ -188,3 +193,76 @@ GROUP BY gm.albumid, gm.album_name, gm.title, gm.description;
     res.status(500).send(error);
   }
 };
+
+export const getGalleryImages = async (req: Request, res: Response) => {
+  const title = req.params.title;
+  try {
+    const galleryMasterRepo = appSource.getRepository(galleryMaster);
+    const details: galleryDetailsDto[] =
+      await galleryMasterRepo.query(`SELECT CAST(gmn.baseimg AS VARCHAR(MAX)) AS baseimg
+FROM [${process.env.DB_name}].[dbo].[gallery_master] gm 
+INNER JOIN [${process.env.DB_name}].[dbo].[gallery_master_nested] gmn 
+    ON gm.albumid = gmn.albumid 
+WHERE gm.isdelete = 1 
+  AND gmn.isdelete = 1
+ `);
+    res.status(200).send({ Result: details });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+
+export const sendMail = async (req: Request, res: Response) => {
+  try {
+    const formDatas = req.body;
+    console.log(formDatas, "email service called");
+    const repo = appSource.getRepository(formDetails);
+    await repo.save(formDatas);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "savedatain@gmail.com",
+        pass: "mqks tltb abyk jlyw",
+      },
+    });
+    await transporter.sendMail({
+      from: "savedatain@gmail.com",
+      to: formDatas.to,
+      subject: `New Inquiry from ${formDatas.fullName}`,
+      text:
+        "Name: " +
+        formDatas.customer_name +
+        "\n" +
+        "Mobile Number: " +
+        formDatas.mobileNumber +
+        "\n" +
+        "Mail-ID: " +
+        formDatas.e_mail +
+        "\n" +
+        "Message: " +
+        formDatas.message,
+    });
+    res.status(200).send({
+      Result: "Mail sent successfully",
+    });
+  } catch (error) {
+    console.log(error, "result");
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+
