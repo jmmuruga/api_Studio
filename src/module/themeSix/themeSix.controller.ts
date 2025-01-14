bae4be32daec14132af3d0a62bd0d250016f1fcc
akshayachuz <akshayamanju2@gmail.com>
2025-01-14 10:20:10 +0530

    changes

--- src/module/themeSix/themeSix.controller.ts
+++ src/module/themeSix/themeSix.controller.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { auth } from "../../shared/helper";
 import {
     getBannerImages
 } from "./themeSix.service";
 
 const themeSixRouter = Router();
-themeSixRouter.get("/themeSixBannerImages/:page", (req, res) => getBannerImages(req, res));
+themeSixRouter.get("/themeSixBannerImages", (req, res) => getBannerImages(req, res));
 
 export default themeSixRouter;
